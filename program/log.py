6dd32037e69d71f14ed9dbfa739a0f3acb53d427
Nour <157008416+NOURNNOOUURR@users.noreply.github.com>
2025-02-12 01:15:38 +0100

    test2

--- program/log.py
+++ program/log.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import *
 
 from PyQt5.QtWidgets import * #QApplication, QMainWindow, QMessageBox, QPushButton, QLabel, QLineEdit, QWidget, QVBoxLayout
 from PyQt5.QtGui import * #QPalette, QColor
+
 class LoginApp(QMainWindow):
     def __init__(self):
         super().__init__()
